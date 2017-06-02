a95f72829951eb60fe4d175a907a3d0f74be144a
Benlolz <benlolz@BenlolzdeMacBook-Pro.local>
2017-06-02 13:48:23 -0700

    change readObject with in while loop

--- src/Broker.java
+++ src/Broker.java
@@ -84,10 +84,7 @@ class BrokerWorker implements Runnable {
                 fwdOut.writeObject(pack2);
 
                 fwdIn = new ObjectInputStream(fwdSock.getInputStream());
-                pack2 = null;
-                while (pack2 == null) {
-                    pack2 = (P2BUp) in.readObject();
-                }
+                pack2 = (P2BUp) in.readObject();
                 fwdSock.close();
 
                 pack2._ack = true;
